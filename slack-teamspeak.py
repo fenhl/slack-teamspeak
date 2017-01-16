d20e6c6dfbb094159923292735cd0bf8ca37b1c8
Fenhl <fenhl@fenhl.net>
2017-01-16 20:16:46 +0000

    Get new users from TeamSpeak

---
+++ slack-teamspeak.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+
+import basedir
+import time
+import ts3
+
+CONFIG = basedir.config_dirs('slack-teamspeak.json').json()
+
+def join(sequence, *, word='and', default=None):
+    sequence = [str(elt) for elt in sequence]
+    if len(sequence) == 0:
+        if default is None:
+            raise IndexError('Tried to join empty sequence with no default')
+        else:
+            return str(default)
+    elif len(sequence) == 1:
+        return sequence[0]
+    elif len(sequence) == 2:
+        return '{} {} {}'.format(sequence[0], word, sequence[1])
+    else:
+        return ', '.join(sequence[:-1]) + ', {} {}'.format(word, sequence[-1])
+
+def update_users(last_users):
+    srv = ts3.TS3Server(CONFIG['hostname'], CONFIG.get('port', 10011), 1)
+    srv.login(CONFIG.get('username', 'serveradmin'), CONFIG['password'])
+    clientlist = srv.clientlist()
+    current_users = {client['client_database_id'] for client in clientlist.values() if client['client_type'] == '0'}
+    new_users = current_users - last_users
+    return sorted(client['client_nickname'] for client in clientlist.values() if client['client_database_id'] in new_users), current_users
+
+if __name__ == '__main__':
+    last_users = set()
+    while True:
+        new_users, last_users = update_users(last_users)
+        print(CONFIG.get('message', '{} joined').format(join(new_users))) #TODO post to Slack
+        time.sleep(CONFIG.get('checkInterval', 5))
